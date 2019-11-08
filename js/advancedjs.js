7e6e3bb731dadc88bc8ee733a44f55b74d8d1530
FloydWatson <floydbcseit@gmail.com>
2019-11-08 14:00:58 +1300

    added search to faq. fixed drag n drop

--- js/advancedjs.js
+++ js/advancedjs.js
@@ -18,19 +18,17 @@ function showPosition(position) {
 }
 
 // drag and drop function
-function allowDrop(ev) {
-    ev.preventDefault();
-}
-
-function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
-}
-
-function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
-}
+    function allowDrop(ev) {
+        ev.preventDefault();
+    }
+    function drag(ev) {
+        ev.dataTransfer.setData("text", ev.target.id);
+    }
+    function drop(ev) {
+        ev.preventDefault();
+        var data = ev.dataTransfer.getData("text");
+        ev.target.appendChild(document.getElementById(data));
+    }
 
 // web storage function
 function storeData() {
--- js/faq.js
+++ js/faq.js
@@ -1,4 +1,4 @@
-
+var myArr;
 //Loads RSS document
 function loadRSS() {
     //Use CORS API website as proxy to retrieve XML file
@@ -11,9 +11,9 @@ function loadRSS() {
     xmlhttp.open("GET", proxy + url, true);
     xmlhttp.send();
     //Check if the entire xml document has been received? If so, process it.
-    xmlhttp.onreadystatechange = function() {
+    xmlhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            var myArr = JSON.parse(this.responseText);
+            myArr = JSON.parse(this.responseText);
             //Load XML document as XML format and process
             processJSON(myArr);
         }
@@ -28,4 +28,26 @@ function processJSON(arr) {
         output += '<div class="advjs"><h3>' + arr[i].question + '</h3><p>' + arr[i].answer + '</p></div>';
     }
     document.getElementById("faqDisplay").innerHTML = output;
+}
+
+// search for questions
+function searchFunction() {
+    var searchTerm = document.getElementById("searchInput").value;
+    var results = myArr.filter(function (myArr) {
+        return myArr.question.indexOf(searchTerm) > -1;
+    });
+
+    //Loop through all results
+    var output = "";
+    document.getElementById("faqDisplay").innerHTML = "";
+    if (results.length == 0) {
+        document.getElementById("faqDisplay").innerHTML = "Not found!";
+    } else {
+        for (var i = 0; i < results.length; i++) {
+            //Display extracted article into the divs
+            output += '<div class="advjs"><h3>' + results[i].question + '</h3><p>' + results[i].answer + '</p></div>';
+        }
+        document.getElementById("faqDisplay").innerHTML = output;
+        
+    }
 }
